472d4d2c5703e0f4ef10644ae27d18ab438706f5
Arie Catsman <catsmanac@outlook.com>
2023-12-29 15:11:34 +0100

    feat: Add stubs to setup Core Enphase Envoy Sensors

---
+++ custom_components/enphase_envoy_custom/switch.py
@@ -0,0 +1,20 @@
+"""Switch Stub for Enphase Envoy solar energy monitor with support for legacy.
+
+This custom integration uses the Enphase Envoy core integration and registers
+an additional updater that reads production data from legacy envoy html pages.
+Code here are stubs required to use the core integration and do not change core.
+"""
+import homeassistant.components.enphase_envoy.select as ee_switch
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+
+# this is a stub and just uses the Enphase_Envoy Core component
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up core enphase envoy integration switch platform."""
+    await ee_switch.async_setup_entry(hass, config_entry, async_add_entities)
