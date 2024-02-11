1d0abccaa233782d77ada11b6ebfaf32e892ad2d
Arie Catsman <catsmanac@outlook.com>
2024-02-11 16:55:33 +0100

    refactor: Add async_setup and async_setup_entry

--- custom_components/enphase_envoy_legacy_support/__init__.py
+++ custom_components/enphase_envoy_legacy_support/__init__.py
@@ -1,16 +1,35 @@
-"""Initialization Stub for Enphase Envoy integration with support for legacy.
+"""Initialization for Enphase Envoy Legacy Support.
 
-This custom integration uses the Enphase Envoy core integration and registers
-an additional updater that reads production data from legacy envoy html pages.
-Code here are stubs required to use the core integration that don't change core
-and a modified async_setup_entry that has added the registration off the
-legacy_updater with the Envoy after it's initiated.
+This custom integration registers an HTML updater with the Pyenphase module used
+by the HA Core Enphase Envoy integration. When registered, the HA Enphase Envoy
+can collect data from legacy Envoy devices with firmware < 3.9 as well.
 """
 from __future__ import annotations
 
+import logging
+
 from pyenphase import register_updater
+from pyenphase.envoy import get_updaters
+from pyenphase.updaters.base import EnvoyUpdater
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 from .legacy_updater import LegacyProductionScraper
 
-# register the updater to get production data from envoy legacy html pages.
-register_updater(LegacyProductionScraper)
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigEntry):
+    """Register the LegacyProductionScraper with Pyenphase module."""
+    updaters: list[type[EnvoyUpdater]] = get_updaters()
+    if LegacyProductionScraper not in updaters:
+        if _LOGGER.level == logging.DEBUG:
+            _LOGGER.debug("Registering LegacyProductionScraper in Pyenphase")
+        register_updater(LegacyProductionScraper)
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Enphase Envoy legacy support from a config entry."""
+    return True
